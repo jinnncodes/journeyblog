7fe74867ebbc38781152ff1495ae2eaebfc6fa66
jinnncodes <genelloydrespensor@gmail.com>
2025-09-23 20:49:00 +0800

    Patches day 3

--- src/assets/imageUrls.ts
+++ src/assets/imageUrls.ts
@@ -16,11 +16,222 @@ export const events: Event[] = [
         ],
   },
   {
+    name: "baihotel",
+    images: [
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245280/2_fndk0b.webp",
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245279/1_nflu7v.webp",
+
+    ],
+  },
+    {
+    name: "bossidolonwheels",
+    images: [
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245280/2_fndk0b.webp",
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245279/1_nflu7v.webp",
+
+    ],
+  },  {
+    name: "burrows",
+    images: [
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245280/2_fndk0b.webp",
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245279/1_nflu7v.webp",
+
+    ],
+  },  {
+    name: "clubtipsy",
+    images: [
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245280/2_fndk0b.webp",
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245279/1_nflu7v.webp",
+
+    ],
+  },  {
+    name: "fatherandson",
+    images: [
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245280/2_fndk0b.webp",
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245279/1_nflu7v.webp",
+
+    ],
+  },  {
+    name: "firstloveteam",
+    images: [
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245280/2_fndk0b.webp",
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245279/1_nflu7v.webp",
+
+    ],
+  },  {
+    name: "firstteamlunch",
+    images: [
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245280/2_fndk0b.webp",
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245279/1_nflu7v.webp",
+
+    ],
+  },  {
+    name: "gettingbank",
+    images: [
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245280/2_fndk0b.webp",
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245279/1_nflu7v.webp",
+
+    ],
+  },  {
+    name: "intro",
+    images: [
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245280/2_fndk0b.webp",
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245279/1_nflu7v.webp",
+
+    ],
+  },  {
+    name: "islandhopping",
+    images: [
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245280/2_fndk0b.webp",
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245279/1_nflu7v.webp",
+
+    ],
+  },  {
+    name: "jhorennakasala",
+    images: [
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245280/2_fndk0b.webp",
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245279/1_nflu7v.webp",
+
+    ],
+  },  {
+    name: "laag",
+    images: [
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245280/2_fndk0b.webp",
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245279/1_nflu7v.webp",
+
+    ],
+  },  {
+    name: "m5",
+    images: [
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245280/2_fndk0b.webp",
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245279/1_nflu7v.webp",
+
+    ],
+  },  {
+    name: "maribago",
+    images: [
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245280/2_fndk0b.webp",
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245279/1_nflu7v.webp",
+
+    ],
+  },  {
+    name: "nagtagayilatrin",
+    images: [
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245280/2_fndk0b.webp",
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245279/1_nflu7v.webp",
+
+    ],
+  },  {
+    name: "naithan",
+    images: [
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245280/2_fndk0b.webp",
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245279/1_nflu7v.webp",
+
+    ],
+  },  {
+    name: "namistailajas",
+    images: [
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245280/2_fndk0b.webp",
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245279/1_nflu7v.webp",
+
+    ],
+  },  {
+    name: "nangloodsjamil",
+    images: [
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245280/2_fndk0b.webp",
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245279/1_nflu7v.webp",
+
+    ],
+  },  {
+    name: "porngods",
+    images: [
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245280/2_fndk0b.webp",
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245279/1_nflu7v.webp",
+
+    ],
+  },  {
+    name: "quarterly",
+    images: [
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245280/2_fndk0b.webp",
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245279/1_nflu7v.webp",
+
+    ],
+  },  {
+    name: "randomshit2",
+    images: [
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245280/2_fndk0b.webp",
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245279/1_nflu7v.webp",
+
+    ],
+  },  {
+    name: "randomshit3",
+    images: [
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245280/2_fndk0b.webp",
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245279/1_nflu7v.webp",
+
+    ],
+  },  {
+    name: "samgyupsaparkmall",
+    images: [
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245280/2_fndk0b.webp",
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245279/1_nflu7v.webp",
+
+    ],
+  },  {
     name: "seda",
     images: [
-      "https://drive.google.com/uc?export=view&id=FILE_ID_1",
-      "https://drive.google.com/uc?export=view&id=FILE_ID_2",
-      // add all seda images here
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245280/2_fndk0b.webp",
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245279/1_nflu7v.webp",
+
+    ],
+  },  {
+    name: "sinulog",
+    images: [
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245280/2_fndk0b.webp",
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245279/1_nflu7v.webp",
+
+    ],
+  },  {
+    name: "sportsfest",
+    images: [
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245280/2_fndk0b.webp",
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245279/1_nflu7v.webp",
+
+    ],
+  },  {
+    name: "teambuilding",
+    images: [
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245280/2_fndk0b.webp",
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245279/1_nflu7v.webp",
+
+    ],
+  },  {
+    name: "tulogakoamigo",
+    images: [
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245280/2_fndk0b.webp",
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245279/1_nflu7v.webp",
+
+    ],
+  },  {
+    name: "upskill",
+    images: [
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245280/2_fndk0b.webp",
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245279/1_nflu7v.webp",
+
+    ],
+  },  {
+    name: "xmasparty",
+    images: [
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245280/2_fndk0b.webp",
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245279/1_nflu7v.webp",
+
+    ],
+  },  {
+    name: "xmasparty2",
+    images: [
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245280/2_fndk0b.webp",
+      "https://res.cloudinary.com/debusvnxa/image/upload/v1758245279/1_nflu7v.webp",
+
     ],
   },
 ];
